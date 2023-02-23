fac3494a774088f0de24a653a4f92ab8d2396811
olezh <olezhka1629@gmail.com>
2023-02-23 13:50:32 +0300

    change registration page

--- ExamEmulator/ExamEmulator/AppShell.xaml.cs
+++ ExamEmulator/ExamEmulator/AppShell.xaml.cs
@@ -1,9 +1,20 @@
-﻿namespace ExamEmulator;
+﻿using ExamEmulator.Pages;
+
+namespace ExamEmulator;
 
 public partial class AppShell : Shell
 {
 	public AppShell()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+        Routing.RegisterRoute("registrationPage", typeof(RegistrationPage));
+        Routing.RegisterRoute("examPage", typeof(ExamPage));
+        Routing.RegisterRoute("addExamPage", typeof(AddExamPage));
+        Routing.RegisterRoute("answerQuestionPage", typeof(AnswerQuestionPage));
+        Routing.RegisterRoute("answerTicketPage", typeof(AnswerTicketPage));
+        Routing.RegisterRoute("generalQuestionPage", typeof(GeneralQuestionPage));
+        Routing.RegisterRoute("genetralTicketPage", typeof(GenetralTicketPage));
+        Routing.RegisterRoute("signInPage", typeof(SignInPage));
+
+    }
 }
